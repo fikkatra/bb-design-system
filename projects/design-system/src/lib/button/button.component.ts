09b04b4a0b508c49b6c92f5f27de851e9faf2ab8
Sophie Traen <sophie_traen@yahoo.com>
2021-09-26 13:41:16 +0200

    fix styling and added button documentation

--- projects/design-system/src/lib/button/button.component.ts
+++ projects/design-system/src/lib/button/button.component.ts
@@ -1,15 +1,27 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ButtonStyle } from './button-style';
 
+/**
+ * A button in the Bytes & Booze branding style.
+ */
 @Component({
   selector: 'ds-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent implements OnInit {
+  /**
+   * The label to display on the button.
+   */
   @Input() label!: string;
+  /**
+   * The style of the button
+   */
   @Input() buttonStyle: ButtonStyle = ButtonStyle.Primary;
 
+  /**
+   * Fired when the space key is tabbed.
+   */
   @Output() spaceKeyUp = new EventEmitter<void>();
 
   constructor() {}
